4307c371aa7fea614d5f11c0b897b629834e1ecd
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-11-23 00:02:11 +0900

    MVC Template - version1: front controller

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -3,6 +3,7 @@ package hello.servlet.web.frontcontroller.v1;
 import hello.servlet.web.frontcontroller.v1.contoller.MemberFormControllerV1;
 import hello.servlet.web.frontcontroller.v1.contoller.MemberListControllerV1;
 import hello.servlet.web.frontcontroller.v1.contoller.MemberSaveControllerV1;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,6 +19,7 @@ public class FrontControllerServletV1 extends HttpServlet {
 
     private Map<String, ControllerV1> controllerMap = new HashMap<>();
 
+    //생성자로 controller mapping 정보를 저장
     public FrontControllerServletV1() {
         controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
         controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
@@ -27,6 +29,17 @@ public class FrontControllerServletV1 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String requestURI = request.getRequestURI(); // URI 정보를 가져옴 /
 
+        //URI를 키로 삼아 객체인스턴스를 받아옴. 다형성에 의해 인터페이스로 객체를 받아올 수 있음 : 부모는 자식을 담을 수 있다
+        ControllerV1 controller = controllerMap.get(requestURI);
+
+        if(controller == null){ // 예외처리
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 해당 컨트롤러로 넘겨줌
+        controller.process(request, response);
     }
 }
